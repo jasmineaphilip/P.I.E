f4b400d707f7a28a8cc3847f71ea22911b2e1322
prparikh06 <44184103+prparikh06@users.noreply.github.com>
2020-02-28 16:41:27 -0500

    commiting v1 of photo uploading

---
+++ Priyas_Corner/cameraRoll/iOS_Authentication/Result.swift
@@ -0,0 +1,35 @@
+//
+//  Result.swift
+//  iOS_Authentication
+//
+//  Created by Kajal  on 2/27/20.
+//  Copyright © 2020 Kajal . All rights reserved.
+//
+
+import Foundation
+public enum Result {
+ case success
+ case failure(Error)
+ 
+ public var isSuccess: Bool {
+   switch self {
+   case .success:
+     return true
+   case .failure:
+     return false
+   }
+ }
+   
+ public var isFailure: Bool {
+   return !isSuccess
+ }
+
+ public var error: Error? {
+   switch self {
+   case .success:
+     return nil
+   case .failure(let error):
+     return error
+   }
+ }
+}
---
+++ Priyas_Corner/cameraRoll/iOS_AuthenticationTests/iOS_AuthenticationTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_AuthenticationTests.swift
+//  iOS_AuthenticationTests
+//
+//  Created by Kajal  on 2/25/20.
+//  Copyright © 2020 Kajal . All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Authentication
+
+class iOS_AuthenticationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
