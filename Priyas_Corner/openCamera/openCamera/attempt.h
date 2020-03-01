5c13be9f639dcbb6143140c7f76485766a1432cb
prparikh06 <44184103+prparikh06@users.noreply.github.com>
2020-03-01 01:11:03 -0500

    added new project

---
+++ Priyas_Corner/openCamera/openCamera/attempt.h
@@ -0,0 +1,16 @@
+//
+//  attempt.h
+//  openCamera
+//
+//  Created by Kajal  on 3/1/20.
+//  Copyright © 2020 Kajal . All rights reserved.
+//
+
+#ifndef attempt_h
+#define attempt_h
+
+#include <stdio.h>
+
+int readSize(int sockfd);
+
+#endif /* attempt_h */
