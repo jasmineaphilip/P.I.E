3218e461d9e2e0ea22091e8314815bf73c2da472
Priya <prparikh06@gmail.com>
2020-04-25 18:04:07 +0000

    testing wordcloud

--- TeamAuth/Servers/Keyword Extraction/cloud.py
+++ TeamAuth/Servers/Keyword Extraction/cloud.py
@@ -1,10 +1,13 @@
 from os import path
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
+from base64 import b64decode, b64encode
 
 text = open(path.join('samplecloud.txt')).read()
 
 wordcloud = WordCloud().generate(text)
+
+wordcloud.to_file('wordcloud.jpg')
 
 plt.imshow(wordcloud)
 
--- TeamAuth/Servers/Keyword Extraction/phrases.py
+++ TeamAuth/Servers/Keyword Extraction/phrases.py
@@ -1,59 +1,11 @@
+from rake_nltk import Rake
 from os import path
-import nltk
-from nltk.corpus import stopwords
+import sys
 
-text = open(path.join('samplephrases.txt')).read()
-# print text
+#uses stopwords for english from NLTK, and all punctuation characters
+r = Rake()
 
-sentence_re = r'(?:(?:[A-Z])(?:.[A-Z])+.?)|(?:\w+(?:-\w+)*)|(?:\$?\d+(?:.\d+)?%?)|(?:...|)(?:[][.,;"\'?():-_`])'
-lemmatizer = nltk.WordNetLemmatizer()
-stemmer = nltk.stem.porter.PorterStemmer()
-grammar = r"""
-    NBAR:
-        {<NN.*|JJ>*<NN.*>}  # Nouns and Adjectives, terminated with Nouns
-        
-    NP:
-        {<NBAR>}
-        {<NBAR><IN><NBAR>}  # Above, connected with in/of/etc...
-"""
-chunker = nltk.RegexpParser(grammar)
-toks = nltk.regexp_tokenize(text, sentence_re)
-postoks = nltk.tag.pos_tag(toks)
-# print postoks
-tree = chunker.parse(postoks)
-stopwords = stopwords.words('english')
+r.extract_keywords_from_text(open(path.join(sys.argv[1])).read())
 
-
-def leaves(tree):
-    """Finds NP (nounphrase) leaf nodes of a chunk tree."""
-    for subtree in tree.subtrees(filter=lambda t: t.label() == 'NP'):
-        yield subtree.leaves()
-
-
-def normalise(word):
-    """Normalises words to lowercase and stems and lemmatizes it."""
-    word = word.lower()
-    # word = stemmer.stem_word(word) #if we consider stemmer then results comes with stemmed word, but in this case word will not match with comment
-    word = lemmatizer.lemmatize(word)
-    return word
-
-
-def acceptable_word(word):
-    """Checks conditions for acceptable word: length, stopword. We can increase the length if we want to consider large phrase"""
-    accepted = bool(2 <= len(word) <= 40)
-    # and word.lower() not in stopwords)
-    return accepted
-
-
-def get_terms(tree):
-    for leaf in leaves(tree):
-        term = [normalise(w) for w, t in leaf if acceptable_word(w)]
-        yield term
-
-
-terms = get_terms(tree)
-
-for term in terms:
-    for word in term:
-        print word,
-    print
+#To get keyword phrases ranked highest to lowest
+print(r.get_ranked_phrases())
